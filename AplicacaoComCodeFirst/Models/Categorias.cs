167c5cf19c02df468dc787dbc37c4b5d929ecb34
julio.souza <julio.souza@WKS22>
2016-09-26 09:32:52 -0300

    Add project files.

---
+++ AplicacaoComCodeFirst/Models/Categorias.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AplicacaoComCodeFirst.Models
+{
+	public class Categorias
+	{
+		[Key]
+		public int CategoriaID { get; set; }
+		public string Categoria { get; set; }
+		public string Descricao { get; set; }
+		public virtual ICollection<Posts>Posts{ get; set; }
+	}
+}
